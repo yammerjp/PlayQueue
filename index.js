dcb5cf12294ee7f58be82e319e766de38d9f746b
Keisuke Nakayama <baskk4.dt@gmail.com>
2018-12-13 14:55:01 +0900

    show relatedmovie/structed value

--- index.js
+++ index.js
@@ -1,180 +1,149 @@
-const TAB_PLAYER =0;
-const TAB_QUEUE  =1;
-const TAB_SEARCH =2;
-const YoutubeKey="AIzaSyCCgdgCh4cw1pnscxtRsfP80M23lMlCdKY";
+const TAB_PLAYER = 0;
+const TAB_QUEUE = 1;
+const TAB_SEARCH = 2;
+const SEARCHED = 0;
+const RELATED = 1;
+const YoutubeKey = "AIzaSyCCgdgCh4cw1pnscxtRsfP80M23lMlCdKY";
 
-const vm =new Vue({
-  el: "#app",
-  data:{
-    movieQueue :[
-      { Id:"etKuJ7ibrvc",
-        description:"お口の恋人ロッテのスペシャルアニメーション 「ベイビーアイラブユーだぜ」のフルバージョンを公開！ 企画・プロデュース:川村元気 監督:松...",
-        thumbnail:"https://i.ytimg.com/vi/etKuJ7ibrvc/default.jpg",
-        title:"ロッテ×BUMP OF CHICKEN ベイビーアイラブユーだぜ フルバージョン",
-        uniqueKey:"1544599827709#0",
-      }
-    ],
-    movieQueueCt : 0,
-
-    searchWord: "",
-    searchWordLS: "",
-    nextPageToken: "",
-    movieSearchList :[],
-    ListClickUniqueKey:"",
-    selectedTab : 1,
-    movable:false,
-    moveFrom:"",
-
-    relatedMovieList:[],
-  },/*
+const vm = new Vue({
+    el: "#app",
+    data: {
+        tabQueue: {
+            mvList: [//movieQueue
+                {
+                    Id: "etKuJ7ibrvc",
+                    description: "お口の恋人ロッテのスペシャルアニメーション 「ベイビーアイラブユーだぜ」のフルバージョンを公開！ 企画・プロデュース:川村元気 監督:松...",
+                    thumbnail: "https://i.ytimg.com/vi/etKuJ7ibrvc/default.jpg",
+                    title: "ロッテ×BUMP OF CHICKEN ベイビーアイラブユーだぜ フルバージョン",
+                    uniqueKey: "1544599827709#0",
+                }],
+            mvListCt: 0,//movieQueueCt
+            move: {
+                able: false,//movable
+                from: '',//moveFrom
+            }
+        },
+        tabPlay: {
+            mvList: [],
+            nextPageToken: '',
+            preWord: 'relatedToVideoId=',
+            wordSubmit: '',// 
+        },
+        tabSearch: {
+            mvList: [],// movieSearchList
+            nextPageToken: '',//nextPageToken:
+            preWord: 'q=',
+            word: '',// searchWord
+            wordSubmit: '',
+        },
+        tabCommon: {
+            ListClickUniqueKey: '',//ListClickUniqueKey
+            selectedTab: 0, // selectedTab
+        },
+    },/*
   mounted(){
   },*/
-  methods:{
-    
-    newMovieQueue:function(msg,movie){
-        switch(msg){
-            case "PLAY_NOW":        
-                vm.movieQueue.splice(vm.movieQueueCt+1,0,movie);
-                playNextMovie()
-                break;
-            case "PLAY_NEXT":
-                vm.movieQueue.splice(vm.movieQueueCt+1,0,movie);
-                break;
-            case "PLAY_LAST":
-                vm.movieQueue.push(movie);
-                break;
+    methods: {
 
-        }
-    },
-    changeMovieQueue(msg,item){
-        const itemCt=vm.movieQueue.findIndex(({uniqueKey})=>uniqueKey===item.uniqueKey);
-        /*↑uniqueキーが一致するmovieQueueの配列番号 つまりitemが存在するmovieQueue配列内の位置 */
-        switch(msg){
-            case "JUMP"://itemの位置に再生キューを移動して再生
-                vm.movieQueueCt= itemCt-1;
-                playNextMovie()
-                break;
-            case 'DELETE'://itemを再生キューから削除
-                if(vm.movieQueueCt==itemCt)//現在再生中なら次を再生してから
-                    playNextMovie();
-                    vm.movieQueue.splice(itemCt,1);//削除
-                break;
+        addMovieQueue: function (msg, movie) {
+            switch (msg) {
+                case "PLAY_NOW":
+                    vm.tabQueue.mvList.splice(vm.tabQueue.mvListCt + 1, 0, movie);
+                    playNextMovie()
+                    break;
+                case "PLAY_NEXT":
+                    vm.tabQueue.mvList.splice(vm.tabQueue.mvListCt + 1, 0, movie);
+                    break;
+                case "PLAY_LAST":
+                    vm.tabQueue.mvList.push(movie);
+                    break;
+
+            }
+        },
+        changeMovieQueue(msg, item) {
+            const itemCt = vm.tabQueue.mvList.findIndex(({ uniqueKey }) => uniqueKey === item.uniqueKey);
+            /*↑uniqueキーが一致するtabQueue.mvListの配列番号 つまりitemが存在するtabQueue.mvList配列内の位置 */
+            switch (msg) {
+                case "JUMP"://itemの位置に再生キューを移動して再生
+                    vm.tabQueue.mvListCt = itemCt - 1;
+                    playNextMovie()
+                    break;
+                case 'DELETE'://itemを再生キューから削除
+                    if (vm.tabQueue.mvListCt == itemCt)//現在再生中なら次を再生してから
+                        playNextMovie();
+                    vm.tabQueue.mvList.splice(itemCt, 1);//削除
+                    break;
+
+                case 'MOVE'://itemの位置を移動して再生キュー内の順番を変更
+                    if (vm.tabQueue.mvListCt == itemCt) {
+                        iziToast.error({
+                            title: 'can not move movie in the list',
+                            message: 'This movie is playing now.'
+                        });
+                        return 0;
+                    }
+
+                    vm.tabQueue.move.from = item;
+                    //削除前に、移動する動画のデータを保存しておく
+                    vm.tabQueue.mvList.splice(itemCt, 1);//削除
+                    if (vm.tabQueue.mvListCt > itemCt)//削除に合わせてtabQueue.mvListCtも現在再生しているものを指すように適切に変更
+                        vm.tabQueue.mvListCt--;
+                    vm.tabQueue.move.able = true;
+                    /*選択画面を挟んでから移動先が決定 */
+                    break;
+
+            }
+        },
+        moveHere(item) {
+            let itemCt;
+            if (item === 0) {
+                itemCt = -1;//一番上に挿入する場合
+            } else {
+                itemCt = vm.tabQueue.mvList.findIndex(({ uniqueKey }) => uniqueKey === item.uniqueKey);
+            }
+            vm.tabQueue.mvList.splice(itemCt + 1, 0, vm.tabQueue.move.from);//予め保存していたデータをリストに挿入
+            if (vm.tabQueue.mvListCt > itemCt)
+                vm.tabQueue.mvListCt++;//挿入後はvm.tabQueue.mvListCtも適切に変更する必要がある
+            vm.tabQueue.move.from = "";
+            vm.tabQueue.move.able = false;
+        },
+
+        searchWordSubmitted: function () {
+            if (this.tabSearch.word == "")
+                return 0;
             
-            case 'MOVE'://itemの位置を移動して再生キュー内の順番を変更
-                if(vm.movieQueueCt==itemCt){
-                    iziToast.error({ 
-                        title: 'This movie is playing now', 
-                        message: 'can not move movie in the list.' 
-                      });
-                    return 0;
+            let active_element = document.activeElement;// フォーカスを外す
+            if(active_element){
+                  active_element.blur();
                 }
-                
-                vm.moveFrom=item;
-                //削除前に、移動する動画のデータを保存しておく
-                vm.movieQueue.splice(itemCt,1);//削除
-                if(vm.movieQueueCt>itemCt)//削除に合わせてmovieQueueCtも現在再生しているものを指すように適切に変更
-                    vm.movieQueueCt--;
-                vm.movable=true;
-                /*選択画面を挟んでから移動先が決定 */
-                break;
+            this.tabSearch.wordSubmit= this.tabSearch.word;
 
-        }
-    },
-    moveHere(item){
-        let itemCt;
-        if(item===0){
-            itemCt=-1;//一番上に挿入する場合
-        }else{
-            itemCt= vm.movieQueue.findIndex(({uniqueKey})=>uniqueKey===item.uniqueKey);
-        }
-        vm.movieQueue.splice(itemCt+1,0,vm.moveFrom);//予め保存していたデータをリストに挿入
-        if(vm.movieQueueCt>itemCt)
-            vm.movieQueueCt++;//挿入後はvm.movieQueueCtも適切に変更する必要がある
-        vm.moveFrom="";
-        vm.movable=false;
-    },
+            this.tabSearch.mvList=[];        //初期化
+            this.tabSearch.nextPageToken=''; //初期化
+            getMovieList(this.tabSearch);
+        },
 
-    searchWordSubmitted:function(){
-      if(this.searchWord=="")
-        return 0;
-      const requestUrl 
-        ='https://www.googleapis.com/youtube/v3/search?'
-        + "q=" + this.searchWord
-        +'&key='+YoutubeKey
-        +'&part=snippet&order=relevance&regionCode=jp&type=video&videoEmbeddable=true';
-      const date = new Date();
+        searchWordSubmittedMore: function () {
+            if (this.tabSearch.wordSubmit == "")
+                return 0;
+            getMovieList(this.tabSearch);
+        },
 
-      axios.get(requestUrl)
-        .then(function (res) {
-          vm.movieSearchList =[];
-          res.data.items.forEach((item,index) => {
-            const searchMovie ={
-                uniqueKey:  `${date.getTime()}#${index}`,
-                Id: item.id.videoId,
-                title: item.snippet.title,
-                description: item.snippet.description,
-                thumbnail: item.snippet.thumbnails.default.url
-              };
-  //            console.log(searchMovie);
-              vm.movieSearchList.push(searchMovie);
-          });
-          vm.nextPageToken=res.data.nextPageToken;
-          vm.searchWordLS = vm.searchWord;
-        
-        }).catch(function (err) {
-          console.log(err);
-          iziToast.error({ 
-            title: 'Reject http request', 
-            message: 'could not get movie details. Youtube reject http request.' 
-          });
-        });
-    },
-
-    searchWordSubmittedMore:function(){
-        if(this.searchWordLS=="")
-            return 0;
-          const requestUrl 
-          ='https://www.googleapis.com/youtube/v3/search?'
-          + "q=" + this.searchWordLS
-          +'&key='+YoutubeKey
-          +'&part=snippet&order=relevance&regionCode=jp&type=video&videoEmbeddable=true'
-          +'&pageToken='+ this.nextPageToken;
-        const date = new Date();
-  
-        axios.get(requestUrl)
-          .then(function (res) {
-            res.data.items.forEach((item,index) => {
-              const searchMovie ={
-                  uniqueKey:  `${date.getTime()}#${index}`,
-                  Id: item.id.videoId,
-                  title: item.snippet.title,
-                  description: item.snippet.description,
-                  thumbnail: item.snippet.thumbnails.default.url
-                };
-                vm.movieSearchList.push(searchMovie);
-            });
-            vm.nextPageToken=res.data.nextPageToken;
-          }).catch(function (err) {
-            console.log(err);
-            iziToast.error({ 
-              title: 'Reject http request', 
-              message: 'could not get movie details. Youtube reject http request.' 
-            });
-        });
-    },
-
-    listMovieClicked:function(movie){
-        if(vm.ListClickUniqueKey==movie.uniqueKey){
-            vm.ListClickUniqueKey = "";
-        }else{
-            vm.ListClickUniqueKey=movie.uniqueKey;
-        }
-    },
-    tabChange(num){
-        vm.ListClickUniqueKey = "";
-        vm.selectedTab=num;
+        listMovieClicked: function (movie) {
+            if (vm.tabCommon.ListClickUniqueKey == movie.uniqueKey) {
+                vm.tabCommon.ListClickUniqueKey = "";
+            } else {
+                vm.tabCommon.ListClickUniqueKey = movie.uniqueKey;
+            }
+        },
+        tabChange(num) {
+            vm.tabCommon.ListClickUniqueKey = "";
+            vm.tabCommon.selectedTab = num;
+        },
+        relatedMovieMore(){
+            getMovieList(this.tabPlay);
+        },
     }
-  }
 });
 
 let tag = document.createElement('script');
@@ -184,52 +153,99 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 let player;
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    videoId: vm.movieQueue[0].Id,
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange,
-      'onError':onPlayerError
-    }
-  });
+    player = new YT.Player('player', {
+        videoId: vm.tabQueue.mvList[0].Id,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange,
+            'onError': onPlayerError
+        }
+    });
 }
 
 function onPlayerReady(event) {
     event.target.playVideo();
+    vm.tabPlay.mvList =[];
+    vm.tabPlay.nextPageToken = '';
+    vm.tabPlay.wordSubmit=vm.tabQueue.mvList[vm.tabQueue.mvListCt].Id;
+    getMovieList(vm.tabPlay);
 }
 
 function onPlayerStateChange(event) {
-  if(event.data == YT.PlayerState.ENDED)
-    playNextMovie();
+    if (event.data == YT.PlayerState.ENDED)
+        playNextMovie();
 }
 
-function onPlayerError(event){
-  switch(event.data){
-    case 2://リクエストが無効なパラメータ値 動画IDのフォーマットが異なる
-    case 5://HTML5プレイヤーに関するエラー
-    case 100://動画が見つからない(非公開含む)
-    case 101://埋め込み不可の動画
-    case 150://埋め込み不可の動画
-      playNextMovie();
-      iziToast.error({ 
-        title: 'Skip Movie', 
-        message: 'The skipped movie is not permitted on Youtube embedded player.'
-      });
-      break;
+function onPlayerError(event) {
+    switch (event.data) {
+        case 2://リクエストが無効なパラメータ値 動画IDのフォーマットが異なる
+        case 5://HTML5プレイヤーに関するエラー
+        case 100://動画が見つからない(非公開含む)
+        case 101://埋め込み不可の動画
+        case 150://埋め込み不可の動画
+            playNextMovie();
+            iziToast.error({
+                title: 'Skip Movie',
+                message: 'The skipped movie is not permitted on Youtube embedded player.'
+            });
+            break;
 
-    default:
-      iziToast.error({ title: 'Unknown Error', message: 'Stop movie' });
-  }
+        default:
+            iziToast.error({ title: 'Unknown Error', message: 'Stop movie' });
+    }
 }
 
-function playNextMovie(){ //MovieQueueが全て再生したら最初からループ
-  vm.movieQueueCt = (vm.movieQueueCt+1) % vm.movieQueue.length;
-//  if(vm.movieQueue.length > vm.movieQueueCt +1 );
+function playNextMovie() { //tabQueue.mvListが全て再生したら最初からループ
+    vm.tabQueue.mvListCt = (vm.tabQueue.mvListCt + 1) % vm.tabQueue.mvList.length;
+    //  if(vm.tabQueue.mvList.length > vm.tabQueue.mvListCt +1 );
     player.loadVideoById({
-      videoId: vm.movieQueue[ vm.movieQueueCt ].Id,
-      suggestedQuality:'small'
+        videoId: vm.tabQueue.mvList[vm.tabQueue.mvListCt].Id,
+        suggestedQuality: 'small'
     });
-//関連動画リストの取得
-
+    //関連動画リストの取得
+    vm.tabPlay.mvList =[];
+    vm.tabPlay.nextPageToken = '';
+    vm.tabPlay.wordSubmit=vm.tabQueue.mvList[vm.tabQueue.mvListCt].Id;
+    getMovieList(mv.tabPlay);
     //relatedToVideoId
+}
+
+function getMovieList(tab) {
+    //vm.movieSearchList =[]; リストの初期化は関数外で行う
+    //nextPageToken=''
+    //wordSubmit=word
+
+    /* tab.preWord+tab.wordSubmit (とnextPageTokenがあればこれも)をGETでyoutubeに送信
+    tab.mvListに動画をpush
+    tab.nextPageTokenを更新
+    */
+    const requestUrl
+        = 'https://www.googleapis.com/youtube/v3/search?'
+        + tab.preWord + tab.wordSubmit
+        + (tab.nextPageToken == '' ? '' : '&pageToken=' + tab.nextPageToken)
+        + '&key=' + YoutubeKey
+        + '&part=snippet&order=relevance&regionCode=jp&type=video&videoEmbeddable=true';
+    const date = new Date();
+
+    axios.get(requestUrl)
+        .then(function (res) {
+            res.data.items.forEach((item, index) => {
+                const searchMovie = {
+                    uniqueKey: `${date.getTime()}#${index}`,
+                    Id: item.id.videoId,
+                    title: item.snippet.title,
+                    description: item.snippet.description,
+                    thumbnail: item.snippet.thumbnails.default.url
+                };
+                tab.mvList.push(searchMovie);
+            });
+            tab.nextPageToken = res.data.nextPageToken;
+
+        }).catch(function (err) {
+            console.log(err);
+            iziToast.error({
+                title: 'Reject http request',
+                message: 'could not get movie details. Youtube reject http request.'
+            });
+        });
 }
